2b0f692232951a2658dd948f729c95e9fbe2706c
oleg <legik7127@gmail.com>
2023-03-05 12:29:33 +0600

    added 3 algorithms of sort

--- src/main/java/Sort.java
+++ src/main/java/Sort.java
@@ -3,7 +3,7 @@ public class Sort {
         int[] array = new int[]{
                 4, 2, 5, 8, 1, 9, 2, 3, 6, 8, 5
         };
-        directSort(array);
+        insertSort(array);
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
@@ -32,10 +32,22 @@ public class Sort {
                     minPosistion = j;
                 }
             }
-            if(i != minPosistion){
+            if (i != minPosistion) {
                 int temp = array[i];
                 array[i] = array[minPosistion];
                 array[minPosistion] = temp;
+            }
+        }
+    }
+
+    public static void insertSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[i] > array[j]){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
             }
         }
     }
