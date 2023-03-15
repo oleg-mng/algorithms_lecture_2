e4f33cdc8867e4ff87690b8c503aa192f38ac5ce
oleg <legik7127@gmail.com>
2023-03-15 09:47:54 +0600

    added 1st algorithm in class Find

--- src/main/java/Sort.java
+++ src/main/java/Sort.java
@@ -26,16 +26,16 @@ public class Sort {
 
     public static void directSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
-            int minPosistion = i;
+            int minPosition = i;
             for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[minPosistion]) {
-                    minPosistion = j;
+                if (array[j] < array[minPosition]) {
+                    minPosition = j;
                 }
             }
-            if (i != minPosistion) {
+            if (i != minPosition) {
                 int temp = array[i];
-                array[i] = array[minPosistion];
-                array[minPosistion] = temp;
+                array[i] = array[minPosition];
+                array[minPosition] = temp;
             }
         }
     }
